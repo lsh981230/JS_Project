e3b7754ecbf84e846c803c9abf4dc0898435c607
이상호 <lsh981230@naver.com>
2022-05-08 01:30:44 +0900

    (이상호) - JS_ProjectGameMode:: connect() 관련 내용 추가

--- Source/JS_Project/JS_Project.Build.cs
+++ Source/JS_Project/JS_Project.Build.cs
@@ -9,5 +9,9 @@ public class JS_Project : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
-	}
+
+        bEnableUndefinedIdentifierWarnings = false;
+
+        PublicAdditionalLibraries.Add("C:/Program Files (x86)/Windows Kits/10/Lib/10.0.20348.0/um/arm/ws2_32.lib");
+    }
 }
--- Source/JS_Project/JS_ProjectGameMode.cpp
+++ Source/JS_Project/JS_ProjectGameMode.cpp
@@ -4,13 +4,80 @@
 #include "JS_ProjectCharacter.h"
 #include "UObject/ConstructorHelpers.h"
 
+//#pragma comment(lib,"ws2_32")
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
 AJS_ProjectGameMode::AJS_ProjectGameMode()
 {
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
-
 		DefaultPawnClass = PlayerPawnBPClass.Class;
+
+
+		//--------------------------------------------------- 
+		// 1. WinSock Init
+		//--------------------------------------------------- 
+
+		WSADATA wsa;
+		if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+			return;
+
+
+
+
+		//--------------------------------------------------- 
+		// 2. socket
+		//--------------------------------------------------- 
+
+		SOCKET _socket = socket(AF_INET, SOCK_STREAM, 0);
+		if (_socket == INVALID_SOCKET)
+		{
+			return;
+		}
+
+
+
+
+		//--------------------------------------------------- 
+		// 3. IP : Port Setting
+		//--------------------------------------------------- 
+
+		SOCKADDR_IN		_serverAddr;
+		_serverAddr.sin_family = AF_INET;
+		InetPton(AF_INET, L"127.0.0.1", &_serverAddr.sin_addr);
+		_serverAddr.sin_port = htons(10001);
+
+
+
+
+		//--------------------------------------------------- 
+		// 4. connect
+		//--------------------------------------------------- 
+
+		for (;;)
+		{
+			if (connect(_socket, (SOCKADDR*)&_serverAddr, sizeof(_serverAddr)) == SOCKET_ERROR)
+			{
+				int a = 0;
+				a++;
+			}
+			else
+				break;
+		}
+
+
+		// Nonblock 소켓으로 변경
+		ULONG isNonBlocking = 1;
+		ioctlsocket(_socket, FIONBIO, &isNonBlocking);
+
+
+		for (;;)
+		{
+
+		}
 	}
 }
